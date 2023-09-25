1d62f1ffd988fa4ec32532c1fda5ced9ad12e907
Hrudananda Barik <hrudanandabarik4@gmail.com>
2023-09-25 12:49:04 +0530

    Added basic layout and integrated to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css';
 
-import { Router } from 'react-router-dom';
-import Footer from './Components/Footer'
+import {Route, Routes } from 'react-router-dom';
+
+import HomePage from './Page/HomePage'
 
 function App() {
   
 
   return (
     <>
-      
+      <Routes>
+           <Route path="/" element={<HomePage />}></Route>
+
+      </Routes>
     </>
   )
 }
